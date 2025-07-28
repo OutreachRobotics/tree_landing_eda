5f3eeb701b09c8cd77f2bdfcbc7bf2fb1574b3cd
chcaya <cayc2401@usherbrooke.ca>
2025-07-28 18:52:59 -0400

    Added a csv combiner

--- python/lib/csv_generator.py
+++ python/lib/csv_generator.py
@@ -4,11 +4,26 @@ from pcl import add_pcl
 from visualizer import viz
 
 import config
+import glob
 import os
 import pandas as pd
 
 
-def generate_csv(_init_idx: int=0, _species: list[str]=[], _should_view: bool=False):
+def combine_csv():
+    path_pattern = os.path.join(config.OUTPUTS_PATH, '*', config.OUTPUT_CSV)
+    all_files = glob.glob(path_pattern)
+
+    if not all_files:
+        print("No CSV files found to combine.")
+        return
+
+    df_list = [pd.read_csv(f) for f in all_files]
+    combined_df = pd.concat(df_list, ignore_index=True)
+    output_path = os.path.join(config.OUTPUTS_PATH, 'output.csv')
+
+    combined_df.to_csv(output_path, index=False)
+
+def generate_csv(_init_idx: int=0, _species: list[str]=[], _should_view: bool=False):    
     for i in range(_init_idx, _init_idx + len(_species)):
         df = pd.DataFrame()
         # df = add_deepforest(df, i)
@@ -16,6 +31,7 @@ def generate_csv(_init_idx: int=0, _species: list[str]=[], _should_view: bool=Fa
         df = add_pcl(df, i, _should_view)
         df['specie'] = _species[i - _init_idx]
 
+        os.makedirs(os.path.join(config.OUTPUTS_PATH, str(i)), exist_ok=True)
         df.to_csv(os.path.join(config.OUTPUTS_PATH, str(i), config.OUTPUT_CSV), index=False)
         print(df)
 
--- python/src/main.py
+++ python/src/main.py
@@ -13,26 +13,18 @@ sys.path.insert(0, str(lib_path))
 print(f"Project root: {project_root}")
 print(f"Current sys.path: {sys.path}")
 
-from csv_generator import generate_csv
+from csv_generator import generate_csv, combine_csv
 
 import config
 
 
 def main():
-   idx = 15
-   specie = 'birch'
+   init_idx = 16
+   species = ['birch', 'birch', 'birch', 'conifer']
+   should_view = False
 
-   init_idx = 12
-   species = ['maple', 'chestnut', 'birch']
-
-   should_view = True
-
-   for i in range(len(species)):
-      os.makedirs(os.path.join(config.OUTPUTS_PATH, str(i)), exist_ok=True)
-
-   # generate_csv(init_idx, species, should_view)
-   generate_csv(idx, specie, should_view)
-
+   generate_csv(init_idx, species, should_view)
+   combine_csv()
 
 if __name__=="__main__":
     main()
